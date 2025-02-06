7f2e2e693dbeeca6f32338839a161d5896d55d5e
yismailsaadelgedawy <youssefgedd@gmail.com>
2025-02-06 00:36:57 +0000

    Major optimisations to the FFT algorithm
    reduced memory usage
    increased processing speed

--- Software/FFT-test/main.cpp
+++ Software/FFT-test/main.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 #include <chrono>
 
-// #define DEBUG
+#define test_frequency 500
 
 // IO
 AnalogIn mic(PA_3); // mic/acc input
@@ -19,11 +19,8 @@ constexpr uint16_t fs = 8192;   // sampling frequency
 constexpr std::chrono::microseconds Ts(1000000/fs); // sampling time period (us)
 
 
-// flags
 
-
-//////////////////////////////////// DFT STUFF ////////////////////////////////////
-
+//////////////////////////////////// FFT STUFF ////////////////////////////////////
 
 using std::complex;
 using std::pow;
@@ -38,17 +35,10 @@ constexpr double f_res = (1.0f)/(N * 1.0f/fs);  // frequency resolution
 complex<double> z3(0,-2*pi/N); // -2*pi*j/N
 complex<double> w = exp(z3);    // twiddle factor, w = e^(-2*pi*j/N)
 
-// symmetry across k=N/2
-// double the speed!
-// half the memory usage!
-constexpr uint16_t symmetry_idx = (N/2)+1;
-
 double x[N]; // input time domain
-complex<double> x_1[N]; // array 1 - time domain (scrambled) -- temporary, will optimise out later!
-complex<double> x_2[N]; // array 2 - time domain
-double X_mag[symmetry_idx]; // used for spectrum estimation
-
-complex<double> W_array[N/2];
+complex<float> x_1[N]; // array 1 - time domain (scrambled) -- temporary, will optimise out later!
+complex<float> x_2[N]; // array 2 - time domain
+complex<float> W_array[N/2];    // twiddle factors (precomputed)
 
 ///////////////////////////////////////////////////////////////////////////////////
 
@@ -60,6 +50,12 @@ void sampling_ISR();
 
 
 int main() {
+
+    // precompute twiddle factors
+    for (int i=0; i<(N/2); i++) {
+        
+        W_array[i] = pow(w,i);  // W^0, W^1, ... W^(N/2 - 1)
+    }
 
     while(1) {
 
@@ -85,16 +81,12 @@ int main() {
             x_1[i] = x[i_scrambled];
         }
 
-        // 2 - precompute twiddle factors
-        for (int i=0; i<(N/2); i++) {
-            
-            W_array[i] = pow(w,i);  // W^0, W^1, ... W^(N/2 - 1)
-        }
+        
 
-        // 3 - perform the FFT
+        // 2 - perform the FFT
         // s: current stage (0 indexed)
         // n: current butterfly (0 indexed)
-        static bool mode = 1;   // initialises once (static local trick)
+        bool mode = 1;
         uint16_t w_idx = 0;
         uint16_t repetitions = (2*N)/8; // number of repeats/holds
         uint16_t rep_cnt = 0;
@@ -152,23 +144,11 @@ int main() {
 
         /////////////////////////////////////////////////////////////
 
-        // obtain the magnitudes, for frequency spectrum
-        if(!mode){
-            for(int k=0; k<symmetry_idx; k++) X_mag[k] = abs(x_2[k]);
-            // for(int i=0; i<N; i++) std::cout << x_2[i] << std::endl; // print output -- DEBUG ONLY
-        }
-        else {
-            for(int k=0; k<symmetry_idx; k++) X_mag[k] = abs(x_1[k]);
-            // for(int i=0; i<N; i++) std::cout << x_1[i] << std::endl; // print output -- DEBUG ONLY
-        }
-
-        // std::cout << std::endl;
-        // for(int i=0; i<N; i++) std::cout << X_mag[i] << std::endl;
-
-        
-
         // print out test frequency
-        std::cout << 1000 << " Hz: " << X_mag[125] << std::endl;
+
+        if(!mode) std::cout << test_frequency << " Hz: " << abs(x_2[test_frequency/(uint16_t)f_res]) << std::endl;
+        else std::cout << test_frequency << " Hz: " << abs(x_1[test_frequency/(uint16_t)f_res]) << std::endl;
+        
 
         
 

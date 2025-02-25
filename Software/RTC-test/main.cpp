cbafa38330f1b9aa90d80ada89b40ec285a25607
yismailsaadelgedawy <youssefgedd@gmail.com>
2025-02-25 17:12:23 +0000

    RTC and related components fully implemented

--- Software/RTC-test/main.cpp
+++ Software/RTC-test/main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 
+// #define DEBUG
+
 ////////////////////////// RTC STUFF //////////////////////////
 
 constexpr uint8_t days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
@@ -28,14 +30,17 @@ Timer btn_tmr;
 
 // time vars
 // globals and statics init to 0
-uint8_t day; uint8_t month; uint16_t year; uint8_t hour; uint8_t minute; uint8_t second;
+uint8_t day = 1; uint8_t month = 1; uint16_t year = 25; uint8_t hour; uint8_t minute;
 
+std::string time_string;    // holds formatted date and time string
+char s1[11];                // fit 11 chars on the first row
+char s2[14];                // fit 14 chars on the second row
 
 ///////////////////////////////////////////////////////////////
 
 // functions
 bool isLeap(uint16_t year);
-time_t unix_stamp(uint8_t d, uint8_t m, uint16_t y, uint8_t hh, uint8_t mm, uint8_t ss);
+time_t unix_stamp(uint8_t d, uint8_t m, uint16_t y, uint8_t hh, uint8_t mm, uint8_t ss=0);
 void lcd_command(unsigned char command);
 void lcd_data(char data);
 void lcd_write(const char* s);
@@ -45,64 +50,231 @@ void lcd_second_line();
 void lcd_init();
 
 
-
-// main() runs in its own thread in the OS
 int main()
 {
 
+    // init LCD
     lcd_init();
-    // lcd_write("Hello!");
-    // lcd_write(255);
 
-    // flag to determine if confirm button has been pressed
-    bool confirm = 0;
+    #ifndef DEBUG
 
-    // setting the day
-    while(1) {
+        // flag to determine if confirm button has been pressed
+        bool confirm = 0;
 
-        lcd_clear();
-        lcd_write("set day: ");
-        lcd_write(day);
+        // setting the day
+        while(1) {
 
-        //////////////// break logic ////////////////
-        if(confirm_btn) {
-            btn_tmr.start();
-            while(confirm_btn) {
+            lcd_clear();
+            lcd_write("set day: ");
+            lcd_write(day);
 
-                if(btn_tmr.elapsed_time() > 200ms) confirm = 1;
-                else {
-                    confirm = 0;
+            //////////////// break logic ////////////////
+            if(confirm_btn) {
+                btn_tmr.start();
+                while(confirm_btn) {
+
+                    if(btn_tmr.elapsed_time() > 200ms) confirm = 1;
+                    else {
+                        confirm = 0;
+                    }
                 }
+                btn_tmr.stop();
+                btn_tmr.reset();
             }
-            btn_tmr.stop();
-            btn_tmr.reset();
+            if(confirm) {
+                while(confirm_btn); // wait for release
+                confirm = 0;        // reset flag
+                break;
+            }
+            /////////////////////////////////////////////
+            
+            if(up_btn) {
+                day++;
+                if(day > 31) day = 1;
+            }        
+            if(down_btn) {
+                day--;
+                if(day == 0) day = 31;
+            }
+            
+            wait_us(250000);
         }
-        if(confirm) break;
-        /////////////////////////////////////////////
-        
-        if(up_btn) day++;            
-        if(down_btn) day--;
-        
-        wait_us(250000);
-    }
+        // setting the month
+        while(1) {
+            
+            lcd_clear();
+            lcd_write("set month: ");
+            lcd_write(month);
+
+            //////////////// break logic ////////////////
+            if(confirm_btn) {
+                btn_tmr.start();
+                while(confirm_btn) {
+
+                    if(btn_tmr.elapsed_time() > 200ms) confirm = 1;
+                    else {
+                        confirm = 0;
+                    }
+                }
+                btn_tmr.stop();
+                btn_tmr.reset();
+            }
+            if(confirm) {
+                while(confirm_btn); // wait for release
+                confirm = 0;        // reset flag
+                break;
+            }
+            /////////////////////////////////////////////
+            
+            if(up_btn) {
+                month++;
+                if(month > 12) month = 1;
+            }           
+            if(down_btn) {
+                month--;
+                if(month == 0) month = 12;
+            }
+            wait_us(250000);
+        }
+        // setting the year
+        while(1) {
+            
+            lcd_clear();
+            lcd_write("set year: ");
+            lcd_write(year);
+
+            //////////////// break logic ////////////////
+            if(confirm_btn) {
+                btn_tmr.start();
+                while(confirm_btn) {
+
+                    if(btn_tmr.elapsed_time() > 200ms) confirm = 1;
+                    else {
+                        confirm = 0;
+                    }
+                }
+                btn_tmr.stop();
+                btn_tmr.reset();
+            }
+            if(confirm) {
+                while(confirm_btn); // wait for release
+                confirm = 0;        // reset flag
+                break;
+            }
+            /////////////////////////////////////////////
+            
+            if(up_btn) year++;            
+            if(down_btn) year--;
+            
+            wait_us(250000);
+        }
+        // setting the hours
+        while(1) {
+            
+            lcd_clear();
+            lcd_write("set hours: ");
+            lcd_write(hour);
+
+            //////////////// break logic ////////////////
+            if(confirm_btn) {
+                btn_tmr.start();
+                while(confirm_btn) {
+
+                    if(btn_tmr.elapsed_time() > 200ms) confirm = 1;
+                    else {
+                        confirm = 0;
+                    }
+                }
+                btn_tmr.stop();
+                btn_tmr.reset();
+            }
+            if(confirm) {
+                while(confirm_btn); // wait for release
+                confirm = 0;        // reset flag
+                break;
+            }
+            /////////////////////////////////////////////
+            
+            if(up_btn) {
+                hour++;
+                if(hour > 23) hour = 0;
+            }            
+            if(down_btn) {
+                hour--;
+                if(hour == 255) hour = 23;
+            }
+            
+            wait_us(250000);
+        }
+        // setting the minutes
+        while(1) {
+            
+            lcd_clear();
+            lcd_write("set minutes: ");
+            lcd_write(minute);
+
+            //////////////// break logic ////////////////
+            if(confirm_btn) {
+                btn_tmr.start();
+                while(confirm_btn) {
+
+                    if(btn_tmr.elapsed_time() > 200ms) confirm = 1;
+                    else {
+                        confirm = 0;
+                    }
+                }
+                btn_tmr.stop();
+                btn_tmr.reset();
+            }
+            if(confirm) {
+                while(confirm_btn); // wait for release
+                confirm = 0;        // reset flag
+                break;
+            }
+            /////////////////////////////////////////////
+            
+            if(up_btn) {
+                minute++;
+                if(minute > 59) minute = 0;
+            }           
+            if(down_btn) {
+                minute--;
+                if(minute == 255) minute = 59;
+            }
+            
+            wait_us(250000);
+        }
+    
 
     
-    // date shall be manually set by user
-    set_time( unix_stamp(25,02,2025,9,0,0) );  // Set RTC time to d/m/y/hh/mm/ss
+        // date shall be manually set by user
+        set_time( unix_stamp(day,month,2000+year,hour,minute) );  // Set RTC time to d/m/y/hh/mm/ss
+    #endif
+
+    #ifdef DEBUG
+        set_time( unix_stamp(1,2,2025,9,0) );  // Set RTC time to d/m/y/hh/mm/ss
+    #endif
 
     while (true) {
         
+        // raw time data stored here
         time_t seconds = time(NULL);
 
-        printf("Time as seconds since January 1, 1970 = %u\n", (unsigned int)seconds);
+        // obtain formatted date and time string
+        time_string = ctime(&seconds);
+        
+        // splicing to fit on LCD
+        for(int i=0; i<11; i++) s1[i] = time_string[i];
+        for(int i=0; i<13; i++) s2[i] = time_string[i+11];
 
-        printf("Time as a basic string = %s", ctime(&seconds));
+        s1[10] = '\0';  // manually place null terminators at the end
+        s2[13] = '\0';  //
 
-        char buffer[32];
-        strftime(buffer, 32, "%I:%M %p\n", localtime(&seconds));
-        printf("Time as a custom formatted string = %s", buffer);
-
-        std::cout << std::endl;
+        // write date and time to LCD
+        lcd_clear();
+        lcd_write(s1);
+        lcd_second_line();
+        lcd_write(s2);
 
         ThisThread::sleep_for(1000ms);
     }
@@ -239,7 +411,7 @@ void lcd_write(const char* s) {
     uint8_t sz = 0;
 
     // determine the size of the string literal passed
-    for(int i=0; i < 8*sizeof(uint8_t); i++) {
+    for(int i=0; i < 255; i++) {
 
         if(s[i] == '\0') break;
         else sz++;
